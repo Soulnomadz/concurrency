ff5ffec97ef965478453c3a1571f456e279d1309
oywz <oywzzc@126.com>
2024-05-17 15:07:08 +0800

    add concurrency support

--- concurrency/examples/metrics.rs
+++ concurrency/examples/metrics.rs
@@ -1,22 +1,66 @@
 use anyhow::Result;
+use rand::Rng;
+
+use std::thread;
+use std::time::Duration;
 
 use concurrency::Metric;
 
+const N: usize = 2;
+const M: usize = 4;
+
 fn main() -> Result<()> {
-    let mut metrics = Metric::new();
-
-    for i in 0..100 {
-        metrics.inc("rbac.web01");
-        metrics.inc("nzp-jxhd-server.app01");
-        if i % 2 == 0 {
-            metrics.inc("oa-jz-shop-server.app06");
-        }
-    }
-
-    for _ in 0..27 {
-        metrics.inc("weixin-server.app02");
-    }
-
+    let metrics = Metric::new();
     println!("{:?}", metrics.snap());
-    Ok(())
+
+    for idx in 0..N {
+        task_worker(idx, metrics.clone());
+    }
+
+    for _ in 0..M {
+        request_worker(metrics.clone());
+    }
+
+    loop {
+        thread::sleep(Duration::from_secs(3));
+        println!("{:?}", metrics.snap());
+    }
+
+    // for i in 0..100 {
+    //     metrics.inc("rbac.web01")?;
+    //     metrics.inc("nzp-jxhd-server.app01")?;
+    //     if i % 2 == 0 {
+    //         metrics.inc("oa-jz-shop-server.app06")?;
+    //     }
+    // }
+
+    // for _ in 0..27 {
+    //     metrics.inc("weixin-server.app02")?;
+    // }
+
+    // println!("{:?}", metrics.snap());
+    // Ok(())
+}
+
+fn task_worker(idx: usize, metrics: Metric) {
+    thread::spawn(move || loop {
+        let mut rng = rand::thread_rng();
+
+        thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
+        metrics.inc(format!("call thread worker: {}", idx)).unwrap();
+    });
+
+    // Ok(())
+}
+
+fn request_worker(metrics: Metric) {
+    thread::spawn(move || loop {
+        let mut rng = rand::thread_rng();
+
+        thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
+        let page_num = rng.gen_range(1..8);
+        metrics.inc(format!("req.page.{}", page_num)).unwrap();
+    });
+    
+    // Ok(())
 }
--- concurrency/src/metrics.rs
+++ concurrency/src/metrics.rs
@@ -1,28 +1,45 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
 
-#[derive(Debug)]
+use anyhow::{Result, anyhow};
+
+#[derive(Debug, Clone)]
 pub struct Metric {
-    data: HashMap<String, i64>,
+    data: Arc<Mutex<HashMap<String, i64>>>,
 }
 
 impl Metric {
     pub fn new() -> Self {
         Self {
-            data: HashMap::new(),
+            data: Arc::new(Mutex::new(HashMap::new())),
         }
     }
 
-    pub fn inc(&mut self, key: &str) {
-        let count = self.data.entry(key.to_string()).or_insert(0);
+    pub fn inc(&self, key: impl Into<String>) -> Result<()> {
+        let mut data = self
+            .data.lock()
+            .map_err(|e| anyhow!("inc error {:?}", e))?;
+
+        let count = data.entry(key.into()).or_insert(0);
         *count += 1;
+
+        Ok(())
     }
 
-    pub fn dec(&mut self, key: &str) {
-        let count = self.data.entry(key.to_string()).or_insert(0);
-        *count -= 1;
-    }
+    // pub fn dec(&mut self, key: &str) {
+    //     let count = self.data.entry(key.to_string()).or_insert(0);
+    //     *count -= 1;
+    // }
 
-    pub fn snap(&self) -> HashMap<String, i64> {
-        self.data.clone()
+    pub fn snap(&self) -> Result<HashMap<String, i64>> {
+        // self.data.clone()
+        Ok(self
+            .data
+            .lock()
+            .map_err(|e| anyhow!("can't get data from metrics: {}", e))?
+            .clone()
+        )
     }
 }
