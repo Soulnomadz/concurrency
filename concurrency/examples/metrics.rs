bad8a59a6a6854993833371298a6a3336d9e932d
oywz <oywzzc@126.com>
2024-05-17 15:20:21 +0800

    fixture: remove closure unwrap

--- concurrency/examples/metrics.rs
+++ concurrency/examples/metrics.rs
@@ -14,53 +14,49 @@ fn main() -> Result<()> {
     println!("{:?}", metrics.snap());
 
     for idx in 0..N {
-        task_worker(idx, metrics.clone());
+        task_worker(idx, metrics.clone())?;
     }
 
     for _ in 0..M {
-        request_worker(metrics.clone());
+        request_worker(metrics.clone())?;
     }
 
     loop {
         thread::sleep(Duration::from_secs(3));
         println!("{:?}", metrics.snap());
     }
-
-    // for i in 0..100 {
-    //     metrics.inc("rbac.web01")?;
-    //     metrics.inc("nzp-jxhd-server.app01")?;
-    //     if i % 2 == 0 {
-    //         metrics.inc("oa-jz-shop-server.app06")?;
-    //     }
-    // }
-
-    // for _ in 0..27 {
-    //     metrics.inc("weixin-server.app02")?;
-    // }
-
-    // println!("{:?}", metrics.snap());
-    // Ok(())
 }
 
-fn task_worker(idx: usize, metrics: Metric) {
-    thread::spawn(move || loop {
-        let mut rng = rand::thread_rng();
+fn task_worker(idx: usize, metrics: Metric) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            let mut rng = rand::thread_rng();
 
-        thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
-        metrics.inc(format!("call thread worker: {}", idx)).unwrap();
+            thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
+            metrics.inc(format!("call thread worker: {}", idx))?;
+        };
+        #[allow(unreachable_code)]
+        // 以下返回仅为避免编译错误
+        Ok::<_, anyhow::Error>(())
     });
 
-    // Ok(())
+    Ok(())
 }
 
-fn request_worker(metrics: Metric) {
-    thread::spawn(move || loop {
-        let mut rng = rand::thread_rng();
+fn request_worker(metrics: Metric) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            let mut rng = rand::thread_rng();
 
-        thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
-        let page_num = rng.gen_range(1..8);
-        metrics.inc(format!("req.page.{}", page_num)).unwrap();
+            thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
+            let page_num = rng.gen_range(1..8);
+            metrics.inc(format!("req.page.{}", page_num))?;
+        };
+        
+        #[allow(unreachable_code)]
+        // 以下返回仅为避免编译错误
+        Ok::<_, anyhow::Error>(())
     });
     
-    // Ok(())
+    Ok(())
 }
